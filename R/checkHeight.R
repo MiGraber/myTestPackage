bf67b451a2f80cb15838a1d9322203c1c4081b13
Michael Graber <m.graber@campus.lmu.de>
2017-11-16 14:54:38 +0100

    completely changed checkheight

--- R/checkHeight.R
+++ R/checkHeight.R
@@ -1,7 +1,7 @@
 # Script for simple function that checks the difference in height from the sex-
 # specific mean for each of the students in the given dataframe
 # Date: 24.10.2017
-# Author: Jann Goschenhofer
+# Author: Tobias Riebe
 # Edit: Michael Graber
 
 #' Return the height difference of the input dataframe withe the mean height of the dataframe
@@ -9,29 +9,48 @@
 #' Return the height difference of the input dataframe withe the mean height of the dataframe.
 #' @param students.input a dataframe with columns: names, heigth, sex
 #' @param sex.specific a boolean which indicates if the height difference is calculated sex specific
+#' @param print.statement a boolean; TRUE prints a message after calculation is done
 #' @return A dataframe with column names and mean height difference
-#' @author Jann Goschenhofer, Michael Graber
+#' @author Tobias Riebe, Michael Graber
 #' @export
 #' @importFrom magrittr %>%
 
-checkHeight = function(students.input = students, sex.specific=TRUE){
+checkHeight <- function(students.input, sex.specific = TRUE, print.statement = FALSE){
 
-  result.frame = data.frame(matrix(NA, nrow = nrow(students.input), ncol = 2))
-  colnames(result.frame) = c("name", "difference")
 
-  male.mean = students.input[,"height"] %>% mean
-  female.mean = students.input[,"height"] %>% mean
-
-  for (i in 1:nrow(students.input)) {
-    # calculate sex-specific deviations from the mean
-    if (students.input[i, "sex"] == "F") {
-      height.diff = 100*(as.numeric(as.vector(students.input[i,]$height)) - female.mean)
-    }
-    else {
-      height.diff = 100*(as.numeric(as.vector(students.input[i, ]$height)) - male.mean)
-    }
-    result.frame[i, "name"] = as.character(students.input[i, "name"])
-    result.frame[i, "difference"] = height.diff
+  #Check if the sex specific height difference or the difference to the whole population should be calculated
+  if(sex.specific == TRUE){
+    #Calculate the gender specific means
+    women_mean_height = students.input[,"height"][students.input[,"sex"] == "F"] %>%
+      mean %>% as.numeric
+    men_mean_height = students.input[,"height"][students.input[,"sex"] == "M"] %>%
+      mean %>% as.numeric
+    #apply a function to the rows of the input dataframe
+    height_vector = apply(students.input, MARGIN = 1,
+                          FUN = function(student){
+                            #substract the gender specific means from the individuals to get height differnces
+                            (if (student["sex"] == "M") men_mean_height - as.numeric(student["height"])
+                             else women_mean_height - as.numeric(student["height"]) )
+                          } )
+    #create the final dataframe containing name od the students and the height differnces
+    #multiple height differences by 100 to get values in cm
+    result.frame = data.frame("name" = students.input$name, "sexspec_height_diff" = height_vector*100)
+  } else{
+    #calculate the mean height of the whole population
+    mean_height = students.input[,"height"] %>% mean %>% as.numeric
+    #apply a function to the rows of the input dataframe
+    height_vector = apply(students.input, MARGIN = 1,
+                          FUN = function(student){
+                            #substract the gender specific means from the individuals to get height differnces
+                            mean_height - as.numeric(student["height"])
+                          } )
+    #create the final dataframe containing name od the students and the height differnces
+    #multiple height differences by 100 to get values in cm
+    result.frame = data.frame("name" = students.input$name, "height_diff" = height_vector*100)
+  }
+  #return the dataframe
+  if(print.statement == TRUE){
+    print("Yippie, I calculated the mean differences!")
   }
   return(result.frame)
 }
