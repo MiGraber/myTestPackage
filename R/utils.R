865b132f83ddeba7e57bf47eaf4eb4954056e2b9
Michael Graber <m.graber@campus.lmu.de>
2017-11-16 15:31:33 +0100

    added arg checks for utils.R

--- R/utils.R
+++ R/utils.R
@@ -1,10 +1,16 @@
 #' Calculates the arithmetic mean
-#' 
+#'
 #' Calculates the arithmetic mean and round on the 3rd digit
 #' @param x a numeric vector
 #' @return an integer
 #' @author Michael Graber
-#' 
-mean = function(x){
-  round(sum(x)/length(x),3)
+#'
+#' @import checkmate
+mean = function(x, digits = 3L){
+  assert(checkInteger(x),
+         checkNumber(x),
+         checkVector(x),
+         combine = "or")
+  assertInt(digits)
+  round(sum(x)/length(x), digits)
 }
